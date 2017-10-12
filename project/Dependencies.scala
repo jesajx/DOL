a3649f012e3925b5d5c4996dd490604d97294e74
jesajx <johannes.olegard@olissoft.com>
2017-10-12 16:50:38 +0200

    fork reactive-async

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val reactiveAsync = ProjectRef(uri("git://github.com/phaller/reactive-async.git#master"), "core")
+  lazy val reactiveAsync = ProjectRef(uri("git://github.com/jesajx/reactive-async.git#master"), "core")
+  //lazy val reactiveAsync = ProjectRef(uri("git:file:///home/jesajx/prg/reactive-async-joh#master"), "core")
 }
